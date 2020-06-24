023d5f6669b006b872b39b3a17fc6a9a37964485
Maicon Boer <maicon_boer@hotmail.com>
2020-06-23 21:52:04 -0300

    adding db config

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,5 +1,4 @@
 import { createConnection } from 'typeorm';
 
-// createConnection() procura no projeto um arquivo chamado
-// ormconfig.json , ler os dados deste arquivo e fazer a conexão com DB
+// createConnection() - procura um arquivo chamado ormconfig.json, lê os dados deste arquivo e faz a conexão com DB
 createConnection();
