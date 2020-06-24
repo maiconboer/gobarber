71cba52aa6c00fcf683a5414bcf31d3b3660295e
Maicon Boer <maicon_boer@hotmail.com>
2020-06-24 17:17:11 -0300

    adding features

---
+++ backend/src/models/User.ts
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    // sem passar parametro - default varchar
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default User;
