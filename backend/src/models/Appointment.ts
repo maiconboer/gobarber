d8ef4c3c12043b7434b93d6137b7799ff0ca9fa6
Maicon Boer <maicon_boer@hotmail.com>
2020-06-24 17:31:25 -0300

    adding features

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,15 +1,29 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+/* eslint-disable camelcase */
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('appointments')
 class Appointment {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column() // sem passar parametro - default varchar
+    // sem passar parametro - default varchar
+    @Column()
     provider: string;
 
     @Column('time without time zone')
     date: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Appointment;
