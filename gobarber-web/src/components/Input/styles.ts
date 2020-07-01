50033d2684c4311640297204ec9a2f482ca815bd
Maicon Boer <maicon_boer@hotmail.com>
2020-07-01 11:43:07 -0300

    adding features

---
+++ gobarber-web/src/components/Input/styles.ts
@@ -0,0 +1,48 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #232129;
+  border-radius: 10px;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  border: 2px solid #232129;
+  color: #666360;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border: 2px solid #ff9000;
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4efe8;
+
+    ::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
---
+++ gobarber-web/src/pages/SignUp/styles.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  width: 100%;
+  max-width: 700px;
+  align-items: center;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: all 0.2s;
+
+      &:hover {
+        filter: brightness(80%);
+      }
+    }
+  }
+
+  /* pegando próximo nível de ancora/link */
+  > a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: all 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      filter: brightness(80%);
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
