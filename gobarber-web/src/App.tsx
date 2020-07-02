e1b7d6de2ce3b646f55f2a7d19b5e87cd286a0c0
Maicon Boer <maicon_boer@hotmail.com>
2020-07-01 22:28:06 -0300

    adding features

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    {/* <SignIn /> */}
-    <SignUp />
+    <AuthContext.Provider value={{ name: 'Maicon' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
