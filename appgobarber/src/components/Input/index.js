53ea89b0648d2d8d0284c65f35e84d7a7db079ed
Maicon Boer <maicon_boer@hotmail.com>
2020-07-17 00:27:31 -0300

    created-input-and-button**login-screen

---
+++ appgobarber/src/components/Input/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, TextInput, Icon } from './styles';
+
+const Input = ({ name, icon, ...rest}) => {
+  return (
+    <Container>
+      <Icon name={icon} size={20} color='#666360'/>
+
+      <TextInput placeholderTextColor='#666360' {...rest}/>
+    </Container>
+  )
+}
+
+export default Input;
