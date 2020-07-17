c40663b3e709427c4db2c133e5cc52f7e6c59729
Maicon Boer <maicon_boer@hotmail.com>
2020-07-16 23:04:36 -0300

    first commit gobarber-mobile

---
+++ appgobarber/src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default SignIn;
