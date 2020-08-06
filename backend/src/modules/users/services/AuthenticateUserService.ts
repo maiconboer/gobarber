d181d146df8bb383d5fcc2b02f3229e14d215bcb
Maicon Boer <maicon_boer@hotmail.com>
2020-08-06 16:55:25 -0300

    Added tests

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,10 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { injectable, inject } from 'tsyringe';
 
 import authConfig from '../../../config/auth';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import AppError from '../../../shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
@@ -24,6 +24,9 @@ class AuthenticateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ email, password }: IRequestDTO): Promise<IResponse> {
@@ -36,7 +39,10 @@ class AuthenticateUserService {
         // user.password - senha criptografada
         // password - senha NÃO criptografada
 
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await this.hashProvider.compareHash(
+            password,
+            user.password,
+        );
 
         if (!passwordMatched) {
             throw new AppError('Incorrect email/password combination.', 401);
